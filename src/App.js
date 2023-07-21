261f2e27b1d3abfdc42715a0bd5c0b71614b7af8
Abdullah <abdullahatik483@gmail.com>
2023-07-21 08:29:52 +0600

    done

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    window.location.href = "https://abdullahmazumder.vercel.app/";
+  }, []);
+  return <div className="App"></div>;
+}
+
+export default App;
